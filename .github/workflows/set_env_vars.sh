095fda942d6a58ef3a83324127192dfd6ee95801
Andrew Marshall <planetmarshalluk@gmail.com>
2021-10-18 01:28:38 +0100

    [ci] build hdf5 if not cached

--- .github/workflows/set_env_vars.sh
+++ .github/workflows/set_env_vars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$1" = "static" ]; then
-	echo 'CONAN_OPTIONS="-o static_plugin=True"' >> $GITHUB_ENV
+	echo "CONAN_OPTIONS=-o static_plugin=True" >> $GITHUB_ENV
 elif [ "$1" = "shared" ]; then
-	echo 'CONAN_OPTIONS="-o static_plugin=True" -o shared=True -o hdf5:shared=True' >> $GITHUB_ENV
+	echo "CONAN_OPTIONS=-o static_plugin=True -o shared=True -o hdf5:shared=True" >> $GITHUB_ENV
 fi
