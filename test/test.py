d03dd1a87fb44125244fc8da84280f2b70643763
Andrew Marshall <planetmarshalluk@gmail.com>
2021-12-12 21:34:07 +0000

    [test] add h5repack roundtrip test

--- test/test.py
+++ test/test.py
@@ -1,6 +1,7 @@
 from subprocess import check_output
 from tempfile import TemporaryDirectory
 import os.path
+import logging
 
 import h5py
 import numpy as np
@@ -19,7 +20,12 @@ def repack(dataset, src_file, dest_file):
     flag = 1
     args = ",".join(str(x) for x in [filter_id, flag])
     cmd = f"h5repack-shared --enable-error-stack -v -f {dataset}:UD={args} {src_file} {dest_file}"
-    return check_output(cmd, shell=True)
+    return check_output(cmd.split())
+
+
+def unpack(dataset, src_file, dest_file):
+    cmd = f"h5repack-shared --enable-error-stack -v -f {dataset}:NONE {src_file} {dest_file}"
+    return check_output(cmd.split())
 
 
 @pytest.mark.parametrize("dataset", ["test8", "test16"])
@@ -40,8 +46,8 @@ def test_compress_greyscale(dataset):
 
     compression_ratio = uncompressed_size / compressed_size
     gzip_compression_ratio = uncompressed_size / gzip_compressed_size
-    print(f"compression ratio: {compression_ratio:.3f}:1")
-    print(f"GZIP compression ratio: {gzip_compression_ratio:.3f}:1")
+    logging.info(f"compression ratio: {compression_ratio:.3f}:1")
+    logging.info(f"GZIP compression ratio: {gzip_compression_ratio:.3f}:1")
     assert compressed_size < gzip_compressed_size
     assert np.count_nonzero(test_raw - test_compressed) == 0
 
@@ -64,8 +70,8 @@ def test_compress_rgb():
 
     compression_ratio = uncompressed_size / compressed_size
     gzip_compression_ratio = uncompressed_size / gzip_compressed_size
-    print(f"compression ratio: {compression_ratio:.3f}:1")
-    print(f"GZIP compression ratio: {gzip_compression_ratio:.3f}:1")
+    logging.info(f"compression ratio: {compression_ratio:.3f}:1")
+    logging.info(f"GZIP compression ratio: {gzip_compression_ratio:.3f}:1")
     assert compressed_size < gzip_compressed_size
     assert np.count_nonzero(test_raw - test_compressed) == 0
 
@@ -84,3 +90,34 @@ def test_backwards_compatibility(dataset_name):
 
     assert compressed_size < original_size
     assert np.count_nonzero(expected_data - data) == 0
+
+
+def _file_size(path):
+    stat = os.stat(os.path.realpath(path))
+    return stat.st_size
+
+
+def test_h5repack_roundtrip():
+    dataset = "test8"
+    with h5py.File(data_file("test.h5")) as h5:
+        expected_data = np.array(h5[dataset])
+
+    logging.info(f"Original file size: {_file_size(data_file('test.h5'))} bytes")
+
+    with TemporaryDirectory() as tempdir:
+        packed_file = os.path.join(tempdir, "test_packed.h5")
+        repack(dataset, data_file("test.h5"), packed_file)
+        packed_file_size = _file_size(packed_file)
+        logging.info(f"Packed file size: {packed_file_size} bytes")
+
+        unpacked_file = os.path.join(tempdir, "test_unpacked.h5")
+        unpack(dataset, packed_file, unpacked_file)
+        unpacked_file_size = _file_size(unpacked_file)
+        logging.info(f"Unpacked file size: {unpacked_file_size} bytes")
+
+        assert(unpacked_file_size > packed_file_size)
+
+        with h5py.File(unpacked_file) as h5:
+            actual_data = np.array(h5[dataset])
+
+    assert np.count_nonzero(expected_data - actual_data) == 0
